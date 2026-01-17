b116dc393f5e60298041786a89e886123ea01e92
raddevus <1radeutsch1@gmail.com>
2026-01-17 13:15:02 -0500

    Moving decryption code to Crypton class

--- AES_Complete/Models/Crypton.cs
+++ AES_Complete/Models/Crypton.cs
@@ -1,7 +1,142 @@
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AES_Complete.Models;
 
 public class Crypton{
    public Crypton(){
    }
+
+
+private string generateHmac(string mackey, string ivAndEncrypted){
+	// NOTE: mackey is lowercased hex values
+	// Example mackey: "c4747607e721580882e7186c136b22d9670779af296772a7abb76f0f40526644"
+	// NOTE: ivAndEncrypted is lowercased in format of iv:encryptedData
+	// Example data: "8c087c023bd23434947a4a477b19dba9:GoHi/mW23ZUwxFkRkbxaCByudTl8FsWw23Yz+KB0ALt9yil8y"
+	byte[] mackeyBytes = StringToBytes(mackey);
+	var hmac = new HMACSHA256(mackeyBytes);
+	var plainText = StringToBytes(ivAndEncrypted);
+	var hmacOut = hmac.ComputeHash(plainText);
+	Console.WriteLine(BytesToHex(hmacOut));
+	return BytesToHex(hmacOut);
+}
+
+static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+{
+    // Check arguments.
+    if (plainText == null || plainText.Length <= 0)
+        throw new ArgumentNullException("plainText");
+    if (Key == null || Key.Length <= 0)
+        throw new ArgumentNullException("Key");
+    if (IV == null || IV.Length <= 0)
+        throw new ArgumentNullException("IV");
+    byte[] encrypted;
+
+    // Create an Aes object
+    // with the specified key and IV.
+    using (Aes aesAlg = AesManaged.Create())
+    {
+		aesAlg.Padding = PaddingMode.PKCS7;
+        aesAlg.Key = Key;
+        aesAlg.IV = IV;
+
+        // Create an encryptor to perform the stream transform.
+        ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+        // Create the streams used for encryption.
+        using (MemoryStream msEncrypt = new MemoryStream())
+        {
+            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            {
+                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                {
+                    //Write all data to the stream.
+                    swEncrypt.Write(plainText);
+                }
+                encrypted = msEncrypt.ToArray();
+            }
+        }
+    }
+
+    // Return the encrypted bytes from the memory stream.
+    return encrypted;
+}
+
+static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+{
+    // Check arguments.
+    if (cipherText == null || cipherText.Length <= 0)
+        throw new ArgumentNullException("cipherText");
+    if (Key == null || Key.Length <= 0)
+        throw new ArgumentNullException("Key");
+    if (IV == null || IV.Length <= 0)
+        throw new ArgumentNullException("IV");
+
+    // Declare the string used to hold
+    // the decrypted text.
+    string plaintext = null;
+
+    // Create an Aes object
+    // with the specified key and IV.
+    using (Aes aesAlg = AesManaged.Create())
+    {
+		aesAlg.Padding = PaddingMode.PKCS7;
+        aesAlg.Key = Key;
+        aesAlg.IV = IV;
+
+        // Create a decryptor to perform the stream transform.
+        ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+        // Create the streams used for decryption.
+        using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+        {
+            using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+            {
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+
+                    // Read the decrypted bytes from the decrypting stream
+                    // and place them in a string.
+                    plaintext = srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+
+    return plaintext;
+}
+
+public static string Base64Encode(string plainText) 
+{
+  var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+  return System.Convert.ToBase64String(plainTextBytes);
+}
+
+public static string Base64Decode(string base64EncodedData) 
+{
+  var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+  return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+}
+
+public static Byte[] Base64DecodeAsBytes(string base64EncodedData) 
+{
+  return System.Convert.FromBase64String(base64EncodedData);
+  //return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+}
+
+private string BytesToHex(byte[] bytes) 
+{ 
+   //return String.Concat(Array.ConvertAll(bytes, x => x.ToString("X2"))); 
+   return String.Concat(Array.ConvertAll(bytes, x => x.ToString("X2"))); 
+}
+
+private byte[] HexStringToBytes(string hex){
+	return System.Convert.FromHexString(hex);
+}
+
+private byte[] StringToBytes(string source){
+	var utf8 = new UTF8Encoding();
+	byte[] targetBytes = utf8.GetBytes(source);
+	return targetBytes;
+}
 }
