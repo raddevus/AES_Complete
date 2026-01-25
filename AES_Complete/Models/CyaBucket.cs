70953ed9092accea3dee2d23f339c6dc83781d14
raddevus <1radeutsch1@gmail.com>
2026-01-25 18:27:32 -0500

    added cyabucket for use with getting data back from LibreStore WebApi

---
+++ AES_Complete/Models/CyaBucket.cs
@@ -0,0 +1,9 @@
+public class CyaBucket {
+   public Int64 Id{get;set;}
+   public String Data{get;set;}
+   public String Hmac{get;set;}
+   public String Iv{get;set;}
+   public DateTime? Created {get;set;}
+   public DateTime? Updated{get;set;}
+   public bool Active{get;set;}
+}
